c08bdac08fd98f32c6fbdfb739dc5d08894763d9
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-03-30 20:02:05 -0300

    Update exercicio 2 ponteiros.c

--- exercicio 2 ponteiros.c
+++ exercicio 2 ponteiros.c
@@ -1,5 +1,5 @@
-/*Faça um programa que use uma estrutura do
-tipo CD e faça a leitura de 10 cds (use um vetor
+/*Usando ponteiros, Faça um programa que use uma estrutura do
+tipo CD e faça a leitura de n cds (use um vetor
 de estrutura do tipo cd). A estrutura CD deve
 conter os seguintes campos:
 Nome da banda
@@ -27,12 +27,12 @@ int main() {
     scanf("%d", &n);
     struct CD *bandas = malloc(n * sizeof(struct CD)); 
     for (int i = 0; i < n; i++) {
-        scanf("%s", (bandas+i)->nome);
-        scanf("%d/%d/%d", &(bandas+i)->dia_lanc, &(bandas+i)->mes_lanc, &(bandas+i)->ano_lanc);
-        scanf("%d/%d/%d", &(bandas+i)->dia_contr, &(bandas+i)->mes_contr, &(bandas+i)->ano_contr);
-        scanf("%f", &(bandas+i)->valor);
-        scanf("%d", &(bandas+i)->num_membros);
-        scanf("%s", (bandas+i)->produtora);
+        scanf("%s", (bandas+i).nome);
+        scanf("%d/%d/%d", &(bandas+i).dia_lanc, &(bandas+i).mes_lanc, &(bandas+i).ano_lanc);
+        scanf("%d/%d/%d", &(bandas+i).dia_contr, &(bandas+i).mes_contr, &(bandas+i).ano_contr);
+        scanf("%f", &(bandas+i).valor);
+        scanf("%d", &(bandas+i).num_membros);
+        scanf("%s", (bandas+i).produtora);
         printf("\n");
     }
 
