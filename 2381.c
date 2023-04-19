3588e20807da96cca532db5df92b27f177a6c097
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-04-19 19:43:48 -0300

    Create 2381.c

---
+++ 2381.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int N,K,i,j;
+  char nomes[100][20],aluno_bonus[21],aux[21];
+  scanf("%d %d", &N, &K);
+  //leitura dos nomes
+  for (i = 0; i < N; i++) {
+    scanf("%s", nomes[i]);
+  }
+  //organização dos nomes por ordem alfabética por seleção
+  for (int i = 0; i < N-1; i++) {
+    int min = i;
+    for (int j = i+1; j < N; j++) {
+      if (strcmp(nomes[j], nomes[min]) < 0) {
+        min = j;
+      }
+    }
+    strcpy(aux, nomes[i]);
+    strcpy(nomes[i], nomes[min]);
+    strcpy(nomes[min], aux);
+  }
+  //identificação e impressão do aluno premiado
+  strcpy(aluno_bonus, nomes[K-1]);
+  printf("%s",aluno_bonus);
+  return 0;
+}
