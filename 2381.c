3b408b8c3e34e403ad5678c5ee92b05f537a0d96
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-04-21 14:28:47 -0300

    Update 2381.c

--- 2381.c
+++ 2381.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(){
-  int N,K,i,j;
-  char nomes[100][21],aluno_bonus[21],aux[21];
-  scanf("%d %d", &N, &K);
-  //leitura dos nomes
-  for (i = 0; i < N; i++) {
-    scanf("%s", nomes[i]);
-  }
-  //organização dos nomes por ordem alfabética usando algoritmo de seleção
-  for (int i = 0; i < N-1; i++) {
-    int min = i;
-    for (int j = i+1; j < N; j++) {
-      if (strcmp(nomes[j], nomes[min]) < 0) {
-        min = j;
-      }
+ 
+int main() {
+ 
+    int N,K,i,j,min;
+    char alunos[100][21],aluno_bonus[21],aux[21];
+    scanf("%d %d",&N,&K);
+    //leitura dos nomes
+    for (i = 0; i < N; i++) {
+        scanf("%s", alunos[i]);
     }
-    strcpy(aux, nomes[i]);
-    strcpy(nomes[i], nomes[min]);
-    strcpy(nomes[min], aux);
-  }
-  //identificação e impressão do aluno premiado
-  strcpy(aluno_bonus, nomes[K-1]);
-  printf("%s",aluno_bonus);
-  return 0;
+    //organização dos nomes por ordem alfabética usando algoritmo de seleção
+    for(i = 0; i < N; i++){
+     min = i;
+     for(j = i+1; j < N; j++)
+       if(strcmp(alunos[j], alunos[min]) < 0)
+        min = j;
+    strcpy(aux,alunos[i]);
+    strcpy(alunos[i],alunos[min]);
+    strcpy(alunos[min],aux);
+    }
+    //identificação e impressão do aluno premiado
+    strcpy(aluno_bonus, alunos[K-1]);
+    printf("%s\n",aluno_bonus);
+    return 0;
+    
 }
