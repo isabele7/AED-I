91e21d07f16cf306c0e2872d4a35233ce11d4f48
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-04-24 19:28:31 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,33 +1,28 @@
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void Insercao (int n, int v[]){
-    int i,j,x;
+void Insercao(int n, int v[]){
+    int i, j, x;
     for(j = 1; j < n; j++){
-      x = v[j];
-      for( i = j-1; i >= 0 && v[i] > x; i--)
-        v[i+1] = v[i];
-      v[i+1] = x;
+        x = v[j];
+        for( i = j-1; i >= 0 && v[i] > x; i--)
+            v[i+1] = v[i];
+        v[i+1] = x;
     }
 }
 
-int main(){
-  
-  int i, n = 20000;
-  int v[n];
-  for (i = 0; i < n; i++) {
+int main() {
+    int n = 20000, i, v[n];
+    for (i = 0; i < n; i++) {
         v[i] = rand();
-  }
-  
-  clock_t start, end;
-  double tempo_gasto; 
-
-  start = clock();
-  Insercao(n, v);
-  end = clock();
-
-  tempo_gasto = ((double) (end - start)) / CLOCKS_PER_SEC;
-  printf("Tempo de execução: %f ms \n", tempo_gasto * 1000);
-  return 0;
+    }
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock(); 
+    Insercao(n, v);
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; 
+    printf("Tempo de execução: %f milissegundos\n", cpu_time_used * 1000);
+    return 0;
 }
