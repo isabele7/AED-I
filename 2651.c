dddee323d5fe7cd73c4f70cd7d73eb01c007c80b
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-05-15 19:37:47 -0300

    Update 2651.c

--- 2651.c
+++ 2651.c
@@ -4,17 +4,19 @@
 
 #define MAX_LENGTH 100001
 
+// Recebe a palavra 'a' e o texto 'b'. Caso essa palavra esteja no texto, ela retorna 1; caso contrário, retorna 0.
 int busca_string(char a[], char b[]) {
     int m = strlen(a), n = strlen(b), i, j;
     for (i = 0; i <= n - m; i++) {
         for (j = 0; j < m; j++) {
+            //a função tolower converte todos os caracteres de entrada de 'b' em minúsculas
             if (tolower(a[j]) != tolower(b[i + j]))
                 break;
         }
         if (j == m)
-            return 1;
+            return 1; //a está em b, a partir da posição i
     }
-    return 0;
+    return 0; //a não está em b
 }
 
 int main() {
