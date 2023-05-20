c8f8120e0a8c075e43330e86bc493a7a69a8e1b6
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-05-20 20:59:31 -0300

    Update 1194.c

--- 1194.c
+++ 1194.c
@@ -28,6 +28,7 @@ int main() {
     return 0;
 }
 
+// Função para construir a árvore binária com base nos percursos infixa e prefixa.  Ela recebe o percurso infixa, o percurso prefixa e o tamanho n da árvore como argumentos. Ela constrói a árvore binária recursivamente dividindo a tarefa em subárvores esquerda e direita com base na posição da raiz no percurso infixa.
 No* construirArvore(char* infixa, char* prefixa, int n) {
     if (n == 0)
         return NULL;
@@ -43,6 +44,7 @@ No* construirArvore(char* infixa, char* prefixa, int n) {
     return raiz;
 }
 
+// Função para imprimir o percurso infixa da árvore. Ela chama recursivamente imprimirInfixa para a subárvore esquerda da raiz, imprimindo os valores dos nós da subárvore esquerda em ordem infixa. Depois, imprime o valor da raiz.. Por fi, chama recursivamente imprimirInfixa para a subárvore direita da raiz, imprimindo os valores dos nós da subárvore direita em ordem infixa.
 void imprimirInfixa(No* raiz) {
     if (raiz == NULL)
         return;
